b4cd6d807a18c681ebbd48569f9e615d25a7ef1e
Sam Selikoff <sam.selikoff@gmail.com>
2019-05-30 12:39:46 -0400

    Episode 4

--- app/pods/1-animated-if/controller.js
+++ app/pods/1-animated-if/controller.js
@@ -1,8 +1,20 @@
 import Controller from '@ember/controller';
 import fade from 'ember-animated/transitions/fade';
 
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
+
 export default Controller.extend({
 
-  transition: fade
+  // transition: fade
+
+  transition: function*({ insertedSprites, removedSprites }) {
+    for (let sprite of insertedSprites) {
+      fadeIn(sprite);
+    }
+
+    for (let sprite of removedSprites) {
+      fadeOut(sprite);
+    }
+  }
 
 });
