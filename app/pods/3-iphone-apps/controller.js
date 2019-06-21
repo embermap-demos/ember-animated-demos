e86cd921ad26cd71141c584565d043d78928a0d9
Sam Selikoff <sam.selikoff@gmail.com>
2019-06-21 12:04:35 -0400

    Episode 9

--- app/pods/3-iphone-apps/controller.js
+++ app/pods/3-iphone-apps/controller.js
@@ -1,9 +1,28 @@
 import Controller from "@ember/controller";
 import { A } from "@ember/array";
+import { fadeOut } from "ember-animated/motions/opacity";
+import move from "ember-animated/motions/move";
+import { wait } from "ember-animated";
+import { isEqual } from "lodash-es";
 
 const COLORS = ["blue", "green", "red"];
 
 export default Controller.extend({
+  *transition({ duration, removedSprites, keptSprites }) {
+    for (let sprite of removedSprites) {
+      yield fadeOut(sprite, { duration: duration * (1 / 4) });
+    }
+
+    let movingSprites = keptSprites.filter(sprite => {
+      return !isEqual(sprite.initialBounds, sprite.finalBounds);
+    });
+
+    for (let sprite of movingSprites) {
+      move(sprite, { duration: duration * (1 / 4) });
+      yield wait(duration * (1 / 2) * (1 / movingSprites.length));
+    }
+  },
+
   init() {
     this._super(...arguments);
 
