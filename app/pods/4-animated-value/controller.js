b363cbb59dba4a553be54c508afd74ef6a470644
Sam Selikoff <sam.selikoff@gmail.com>
2019-06-25 17:03:00 -0400

    Episode 10

--- app/pods/4-animated-value/controller.js
+++ app/pods/4-animated-value/controller.js
@@ -1,8 +1,27 @@
 import Controller from "@ember/controller";
 import faker from "faker";
+// import { toLeft } from "ember-animated/transitions/move-over";
+import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
+import move from "ember-animated/motions/move";
+import { wait } from "ember-animated";
 
 export default Controller.extend({
-  *transition() {},
+  *transition({ duration, insertedSprites, removedSprites }) {
+    for (let sprite of removedSprites) {
+      sprite.endAtPixel({ x: 0 - sprite.initialBounds.width });
+      move(sprite, {
+        duration: duration * (1 / 2)
+      });
+      fadeOut(sprite);
+    }
+
+    for (let sprite of insertedSprites) {
+      sprite.startAtPixel({ x: window.innerWidth });
+      move(sprite, {
+        duration: duration * (1 / 2)
+      });
+    }
+  },
 
   word: faker.company.bsBuzz().toUpperCase(),
 
