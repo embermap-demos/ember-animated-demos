61e9e1cf01cc0368068cfd6aa37b39ca2238d0bf
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-15 13:32:49 -0400

    wip

--- app/pods/8-nested-routes/index/controller.js
+++ app/pods/8-nested-routes/index/controller.js
@@ -3,14 +3,40 @@ import { A } from "@ember/array";
 import { inject as service } from "@ember/service";
 import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
 import move from "ember-animated/motions/move";
-import { wait } from "ember-animated";
+import scale from "ember-animated/motions/scale";
+import { wait, Motion } from "ember-animated";
 import resize from "ember-animated/motions/resize";
+
+function freeze(sprite, opts) {
+  return new Wait(sprite, opts).run();
+}
+
+class Wait extends Motion {
+  *animate() {
+    yield wait(this.duration);
+  }
+}
 
 export default Controller.extend({
   appState: service(),
   router: service(),
 
-  *transition({ duration, removedSprites }) {
+  *transition({ duration, removedSprites, insertedSprites, receivedSprites }) {
+    yield Promise.all(
+      insertedSprites.map(sprite => {
+        sprite.applyStyles({ opacity: 0 });
+        return freeze(sprite, { duration: duration * (3 / 4) });
+      })
+    );
+
+    insertedSprites.forEach(sprite => {
+      fadeIn(sprite, { duration: duration * (1 / 4) });
+    });
+
+    // receivedSprites.forEach(sprite => {
+    //   sprite.moveToFinalPosition();
+    // });
+
     for (let sprite of removedSprites) {
       fadeOut(sprite, { duration: duration * (1 / 4) });
     }
--- app/pods/8-nested-routes/word/controller.js
+++ app/pods/8-nested-routes/word/controller.js
@@ -2,6 +2,7 @@ import Controller from "@ember/controller";
 import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
 import move from "ember-animated/motions/move";
 import adjustCSS from "ember-animated/motions/adjust-css";
+import adjustColor from "ember-animated/motions/adjust-color";
 import resize from "ember-animated/motions/resize";
 import scale from "ember-animated/motions/scale";
 import { wait } from "ember-animated";
@@ -22,48 +23,54 @@ class Wait extends Motion {
 export default Controller.extend({
   appState: service(),
 
-  *parentTransition({ duration, receivedSprites }) {
+  *parentTransition({ duration, receivedSprites, sentSprites }) {
     for (let sprite of receivedSprites) {
       sprite.applyStyles({
         width: sprite.initialBounds.width,
         height: sprite.initialBounds.height
       });
+      // debugger;
       sprite.element.querySelector(
         "div"
       ).style.width = `${sprite.finalBounds.width}px`;
-      // sprite.reveal();
-      // yield wait(duration * (1 / 8));
+
+      // Delay by 1/8 duration to give removed sprites chance to fade out
       yield freeze(sprite, { duration: duration / 8 });
 
-      // let { left, top } = sprite.absoluteInitialBounds;
-      // sprite.startAtPixel({ x: left, y: top });
-      // sprite.startTranslatedBy(0, 30);
+      resize(sprite, { duration: duration * (7 / 8) });
+      move(sprite, { duration: duration * (7 / 8) });
+    }
+
+    console.log("parent ", arguments[0]);
+
+    for (let sprite of sentSprites) {
+      // sprite.applyStyles({
+      //   width: sprite.initialBounds.width,
+      //   height: sprite.initialBounds.height
+      // });
+      // sprite.element.querySelector(
+      //   "div"
+      // ).style.width = `${sprite.finalBounds.width}px`;
+      // yield freeze(sprite, { duration: duration / 8 });
+      //
       resize(sprite, { duration: duration * (7 / 8) });
       move(sprite, { duration: duration * (7 / 8) });
     }
   },
 
-  *rest({ duration, receivedSprites }) {
+  *rest({ duration, receivedSprites, sentSprites, removedSprites }) {
     for (let sprite of receivedSprites) {
-      // move(sprite);
       yield wait(duration * (1 / 2));
       sprite.moveToFinalPosition();
       fadeIn(sprite, { from: 0, duration: duration * (1 / 2) });
-      // debugger;
     }
-    // console.log(arguments[0]);
-  },
 
-  *titleTransition({ beacons, duration, receivedSprites }) {
-    for (let sprite of receivedSprites) {
-      // sprite.startTranslatedBy(0, 30);
-      // debugger;
-      // yield wait(duration);
-      // sprite.reveal();
-      // sprite.applyStyles({ opacity: 0 });
-      // debugger;
-      // resize(sprite);
-      // move(sprite);
+    console.log("child ", arguments[0]);
+
+    for (let sprite of removedSprites) {
+      // freeze(sprite);
+      //   sprite.applyStyles({ zIndex: 99 });
+      //   fadeOut(sprite, { from: 1, to: 0, duration: duration * (1 / 8) });
     }
   }
 });
