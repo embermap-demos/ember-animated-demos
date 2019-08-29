7cfacfc7de4a2cdf8bbbb6e25ce9864673358f3b
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-29 08:55:07 -0400

    remove old stuff

--- app/helpers/marked.js
+++
@@ -1,9 +0,0 @@
-import { helper } from "@ember/component/helper";
-import _marked from "marked";
-import { htmlSafe } from "@ember/string";
-
-export function marked(params /*, hash*/) {
-  return htmlSafe(`<div class='markdown'>${_marked(params[0])}</div>`);
-}
-
-export default helper(marked);
