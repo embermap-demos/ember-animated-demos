94368b8989d6b04e8140d60e6f9e993fbab20063
Sam Selikoff <sam.selikoff@gmail.com>
2019-09-13 15:31:08 -0400

    Episode 19

--- app/pods/8-nested-routes/movie/controller.js
+++ app/pods/8-nested-routes/movie/controller.js
@@ -1,13 +1,35 @@
 import Controller from "@ember/controller";
 import move from "ember-animated/motions/move";
 import resize from "ember-animated/motions/resize";
+import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
+import { wait } from "ember-animated";
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
+  appState: service(),
+
   *transition({ receivedSprites }) {
     console.log("detail: ", arguments[0]);
     receivedSprites.forEach(sprite => {
+      sprite.applyStyles({
+        zIndex: 1
+      });
+
       resize(sprite);
       move(sprite);
+    });
+  },
+
+  *fade({ duration, insertedSprites, removedSprites }) {
+    console.log("fade: ", arguments[0]);
+    removedSprites.forEach(sprite => {
+      fadeOut(sprite, { duration: duration / 2 });
+    });
+
+    yield wait(duration / 2);
+
+    insertedSprites.forEach(sprite => {
+      fadeIn(sprite, { duration: duration / 2 });
     });
   }
 });
--- app/pods/8-nested-routes/movies/controller.js
+++ app/pods/8-nested-routes/movies/controller.js
@@ -3,12 +3,13 @@ import move from "ember-animated/motions/move";
 import resize from "ember-animated/motions/resize";
 import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
 import { inject as service } from "@ember/service";
+import { wait } from "ember-animated";
 
 export default Controller.extend({
   appState: service(),
 
-  *transition({ receivedSprites, insertedSprites, removedSprites }) {
-    console.log("index: ", arguments[0]);
+  *transition({ duration, receivedSprites, insertedSprites, removedSprites }) {
+    // console.log("index: ", arguments[0]);
     receivedSprites.forEach(sprite => {
       sprite.applyStyles({
         zIndex: 1
@@ -18,11 +19,13 @@ export default Controller.extend({
     });
 
     removedSprites.forEach(sprite => {
-      fadeOut(sprite);
+      fadeOut(sprite, { duration: duration / 2 });
     });
 
+    yield wait(duration / 2);
+
     insertedSprites.forEach(sprite => {
-      fadeIn(sprite);
+      fadeIn(sprite, { duration: duration / 2 });
     });
   }
 });
