359c50ab6dbe0d12548fcf5fda8645338d045f7b
Sam Selikoff <sam.selikoff@gmail.com>
2019-09-13 15:27:53 -0400

    Episode 18

--- app/pods/8-nested-routes/movies/controller.js
+++ app/pods/8-nested-routes/movies/controller.js
@@ -1,24 +1,28 @@
 import Controller from "@ember/controller";
 import move from "ember-animated/motions/move";
 import resize from "ember-animated/motions/resize";
-import { fadeIn } from 'ember-animated/motions/opacity';
-import { inject as service } from '@ember/service';
+import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   appState: service(),
 
-  *transition({ receivedSprites, insertedSprites }) {
+  *transition({ receivedSprites, insertedSprites, removedSprites }) {
     console.log("index: ", arguments[0]);
     receivedSprites.forEach(sprite => {
       sprite.applyStyles({
         zIndex: 1
-      })
+      });
       resize(sprite);
       move(sprite);
     });
 
+    removedSprites.forEach(sprite => {
+      fadeOut(sprite);
+    });
+
     insertedSprites.forEach(sprite => {
-      fadeIn(sprite)
-    })
+      fadeIn(sprite);
+    });
   }
 });
